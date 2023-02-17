7b6fd335414ab4b801fd27299a3e2b1cd4259324
sooraj <sooraj@mozilor.com>
2023-02-17 17:55:03 +0530

    script

--- app/layout.js
+++ app/layout.js
@@ -13,12 +13,42 @@ export default function RootLayout({ children }) {
             })(window,document,'script','dataLayer','GTM-M832272');
           `,
           }}
+          onLoad={() => {
+            (function (w, d, s, l, i) {
+              w[l] = w[l] || [];
+              w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+              var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != "dataLayer" ? "&l=" + l : "";
+              j.async = true;
+              j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+              f.parentNode.insertBefore(j, f);
+            })(window, document, "script", "dataLayer", "GTM-M832272");
+            (function (h, o, t, j, a, r) {
+              h.hj =
+                h.hj ||
+                function () {
+                  (h.hj.q = h.hj.q || []).push(arguments);
+                };
+              h._hjSettings = { hjid: 3371256, hjsv: 6 };
+              a = o.getElementsByTagName("head")[0];
+              r = o.createElement("script");
+              r.async = 1;
+              r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+              a.appendChild(r);
+            })(
+              window,
+              document,
+              "https://static.hotjar.com/c/hotjar-",
+              ".js?sv="
+            );
+          }}
         />
         {/* <script
           id="cookieyes"
           src="https://scriptstaging.cookieyes.com/client_data/41dbb457e7b5a3e04556e5d1/script.js"
         /> */}
-        <Script
+        {/* <Script
           strategy="lazyOnload"
           dangerouslySetInnerHTML={{
             __html: `
@@ -44,7 +74,7 @@ export default function RootLayout({ children }) {
           })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
           `,
           }}
-        />
+        /> */}
       </head>
       <body>
         {children}
