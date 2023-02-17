782c9199743378603050b5a1e1aff52e48793d5a
sooraj <sooraj@mozilor.com>
2023-02-17 17:51:48 +0530

    script

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script
+        <Script
           dangerouslySetInnerHTML={{
             __html: `
             (function(w,d,s,l,i){var f=d.getElementsByTagName(s)[0],
