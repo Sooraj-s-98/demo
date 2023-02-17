bcee9ea43adaf12369cb7b5a73ee92ac96a15d1b
sooraj <sooraj@mozilor.com>
2023-02-17 16:47:43 +0530

    script

--- app/layout.js
+++ app/layout.js
@@ -4,21 +4,20 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script
-          id="cookieyes"
-          src="https://scriptstaging.cookieyes.com/client_data/41dbb457e7b5a3e04556e5d1/script.js"
-        />
-        <script
+      <script
           dangerouslySetInnerHTML={{
             __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            (function(w,d,s,l,i){var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s);j.id='cookieyes'; j.src=
+            'https://scriptstaging.cookieyes.com/client_data/41dbb457e7b5a3e04556e5d1/script.js';f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-M832272');
           `,
           }}
         />
+        {/* <script
+          id="cookieyes"
+          src="https://scriptstaging.cookieyes.com/client_data/41dbb457e7b5a3e04556e5d1/script.js"
+        /> */}
 
         <script
           dangerouslySetInnerHTML={{
